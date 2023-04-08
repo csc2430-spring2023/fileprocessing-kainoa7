b56211cd2ece11bc3745654ce08579bdc41d486e
kainoa7 <aquic@spu.edu>
2023-04-07 23:12:00 -0700

    Update Format.cpp
    Added code to find Height and Nationality

--- Formatting.cpp
+++ Formatting.cpp
@@ -69,7 +69,7 @@ string FormatAsCSV(string json) {
 
     if (!foundFirst) {
         firstName = "";
-    } //this finds the first name
+    } //*above code: this finds the first name and stores it into a string. returns blank if not found.
 
 // Pick up from here: do the same thing for lastName, age, and nationality as I did for the code above *checkpoint
 
@@ -101,7 +101,7 @@ string FormatAsCSV(string json) {
         lastName = "";
     }
 
-    // finding person's last name if they have one and putting it into a string. blank if it doesn't exist.
+    // *above code: finding person's last name if they have one and putting it into a string. blank if it doesn't exist.
 
     string ageNum = "Age";
     size_t foundAge = json.find(ageNum);
@@ -133,7 +133,69 @@ string FormatAsCSV(string json) {
     if (!foundAge) {
         age = "";
     }
-    format = firstName + comma + lastName + comma + age + comma + height + comma + nationality;;
+    // *above code: finds persons age if they have on and puts it into a string and blank if not
+
+    string heightNum = "Height";
+    size_t foundHeight = json.find(heightNum);
+
+
+    if (foundHeight!=std::string::npos) {
+        for (int i = foundHeight + 8; i < length; i++){
+            if (!isdigit(json.at(i)) && json.at(i) != ',') {
+                continue;
+
+            }
+
+            else if (isdigit(json.at(i))) {
+
+                height += json.at(i);
+
+
+            }
+
+            else {
+                break;
+            }
+
+        }
+
+
+    }
+
+    if (!foundHeight) {
+        height = "";
+    }
+
+    // *above code: finds height and puts it into a string. string is blank if height doesn't exist.
+
+    string nationalityName = "Nationality";
+    size_t foundNationality = json.find(nationalityName);
+
+
+    if (foundNationality!=std::string::npos) {
+        for (int i = foundNationality + 14; i < length; i++){
+            if (!isalpha(json.at(i)) && json.at(i) != '"') {
+                continue;
+            }
+
+            else if (isalpha(json.at(i))) {
+
+                nationality += json.at(i);
+            }
+
+            else {
+                break;
+            }
+        }
+    }
+
+    if (!foundNationality) {
+        nationality = "";
+    }
+
+    // *above code: finds nationality and puts it into a string. string is blank if nationality doesn't exist.
+
+    format = firstName + comma + lastName + comma + age + comma + height + comma + nationality;
 
     return format;
 }
